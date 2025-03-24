bf794409a841c610ec2a2080fea3c45d47f3cb68
Lana Harkusha <svetlanaharkusha@gmail.com>
2025-03-24 12:00:41 +0100

    Remove deleted files

--- Document8.js
+++
@@ -1,8 +0,0 @@
-const secondsInMinute = 60;
-const secondsInHour = secondsInMinute*60;
-const secondsInDay = secondsInHour*24;
-const secondsInMonth = secondsInDay*30;
-
-console.log(secondsInHour);
-console.log(secondsInDay);
-console.log(secondsInMonth);
